cf46f31713d6856b94a0a4a354d7cd3089d7e67b
AnzePecaver <apecaver@gmail.com>
2022-02-24 17:19:35 +0100

    Izracun in izpis gravitacijskega pospeska

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,7 +1,17 @@
+import java.util.*;
+
 public class Gravitacija {
 	
 	public static void main(String[] args) {
 		System.out.print("OIS je zakon!");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		double nadmorskaVisina = scan.nextDouble();
+		
+		double gravitacijskiPospesek = (6.674 * Math.pow(10.0, -11.0) * 5.972 * Math.pow(10.0, 24.0)) / Math.pow(nadmorskaVisina + 6371000.0, 2.0);
+		
+		System.out.print(gravitacijskiPospesek);
 	}
 	
 }
