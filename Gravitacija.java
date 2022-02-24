8ddc4de6f53b5ad1735553c514ebb0902bf74214
jerca-t <jerca.trcek@gmail.com>
2022-02-24 18:28:33 +0100

    dodan izpis v konzolo dva

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,7 +1,10 @@
 public class Gravitacija {
-	
+
 	public static void main(String[] args) {
 		System.out.print("OIS je zakon!");
 	}
-	
+	public static void print(double visina, double pospesek){
+			System.out.print("Nadmorska visina je" + visina + "gravitacijski pospesek pa" + pospesek);
+	}
+
 }
